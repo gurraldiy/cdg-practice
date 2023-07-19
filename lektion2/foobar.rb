527e68900bca488f938eab131884c37b038ba7b6
gurraldiy <gurraldiy@internet.ru>
2023-07-19 13:36:32 +0900

    Add foobar.rb

---
+++ lektion2/foobar.rb
@@ -0,0 +1,15 @@
+def foobar(a, b)
+  if a == 20
+    return b
+  elsif
+    b == 20
+    return a
+  else
+    return a + b
+  end
+end
+
+puts foobar(11, 20)
+puts foobar(20, 22)
+puts foobar(20, 20)
+puts foobar(33, 44)
